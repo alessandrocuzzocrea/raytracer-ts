6b30de54c54d93c9fb8fe8c614974063bc4d3847
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-06-26 08:19:24 +0900

    put Viewport class in his own separate file

---
+++ src/viewport.ts
@@ -0,0 +1,25 @@
+class Viewport {
+    private width: number
+    private height: number
+    private canvas: HTMLCanvasElement
+    private context: CanvasRenderingContext2D
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+
+    init(document: Document) {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.context = this.canvas.getContext('2d');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.clear();
+    }
+
+    clear() {
+        this.context.fillStyle = 'gray';
+        this.context.fillRect(0, 0, this.width, this.height);
+    }
+}
