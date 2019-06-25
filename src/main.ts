cf65a75f946562b2fca746e162e70d7023205d25
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-06-26 08:06:32 +0900

    add Viewport class

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-console.log('hello from typescript');
-let canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.width = 100;
-canvas.height = 100;
-canvas.getContext('2d').fillStyle = 'red';
-canvas.getContext('2d').fillRect(0, 0, 100, 100);
+class Viewport {
+    width: number
+    height: number
+    canvas: HTMLCanvasElement
+    context: CanvasRenderingContext2D
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+
+    init(document: Document) {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.context = this.canvas.getContext('2d');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.clear();
+    }
+
+    clear() {
+        this.context.fillStyle = 'gray';
+        this.context.fillRect(0, 0, this.width, this.height);
+    }
+}
+
+let viewport = new Viewport(640, 480);
+viewport.init(document);
