832d5afcedccc5235427154d55c4bda95431b916
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2019-06-26 08:06:32 +0900

    draw red square

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 console.log('hello from typescript');
+let canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+canvas.width = 100;
+canvas.height = 100;
+canvas.getContext('2d').fillStyle = 'red';
+canvas.getContext('2d').fillRect(0, 0, 100, 100);
