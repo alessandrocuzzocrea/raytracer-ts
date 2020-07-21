8f186eaab56aac360d2018adebc2f35d71fc39fe
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-21 21:26:41 +0900

    Fix vec3.test.ts indentation

--- test/vec3.test.ts
+++ test/vec3.test.ts
@@ -1,20 +1,20 @@
 import { Vec3 } from '../src/vec3'
 
 test('Initialize the vector', () => {
-  let u = new Vec3(0, 0, 0);
-  expect(u.x).toBe(0);
-  expect(u.y).toBe(0);
-  expect(u.z).toBe(0);
+    let u = new Vec3(0, 0, 0);
+    expect(u.x).toBe(0);
+    expect(u.y).toBe(0);
+    expect(u.z).toBe(0);
 });
 
 test('Zero Vector', ()=>{
-  let u = Vec3.Zero();
-  expect(u.x).toBe(0);
-  expect(u.y).toBe(0);
-  expect(u.z).toBe(0);
+    let u = Vec3.Zero();
+    expect(u.x).toBe(0);
+    expect(u.y).toBe(0);
+    expect(u.z).toBe(0);
 });
 
 test('Returns vector length (magnitude)', ()=>{
-  let u = new Vec3(1, 1, 1);
-  expect(u.Length()).toBeCloseTo(1.7320508075688772)
+    let u = new Vec3(1, 1, 1);
+    expect(u.Length()).toBeCloseTo(1.7320508075688772)
 });
