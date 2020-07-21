7a4f9adf6b73634c41e917b1339006d7f994d7cf
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-21 21:17:26 +0900

    Add a couple of assertions to "Initialize the vector" test

--- test/vec3.test.ts
+++ test/vec3.test.ts
@@ -3,4 +3,7 @@ import { Vec3 } from '../src/vec3'
 test('Initialize the vector', () => {
   let u = new Vec3(0, 0, 0);
   expect(u.x).toBe(0);
+  expect(u.y).toBe(0);
+  expect(u.z).toBe(0);
+});
 });
