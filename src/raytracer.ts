87e0b57142ad491aa165346325225295f45ee758
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-28 01:13:31 +0900

    Weird stuff

--- src/raytracer.ts
+++ src/raytracer.ts
@@ -6,6 +6,7 @@ export class Raytracer {
     private div: HTMLDivElement;
     private clearButton: HTMLButtonElement;
     private fillRandomButton: HTMLButtonElement;
+    private renderButton: HTMLButtonElement;
 
     constructor(width: number, height: number) {
         this.div = document.createElement('div');
@@ -21,6 +22,11 @@ export class Raytracer {
         this.fillRandomButton.innerText = 'fillRandom';
         this.fillRandomButton.onclick = () => this.fillRandom();
         this.div.appendChild(this.fillRandomButton);
+
+        this.renderButton = document.createElement('button');
+        this.renderButton.innerText = 'render';
+        this.renderButton.onclick = () => this.render();
+        this.div.appendChild(this.renderButton);
     }
 
     clear() {
@@ -29,5 +35,9 @@ export class Raytracer {
 
     fillRandom() {
         this.viewport.fillRandom();
+    }
+
+    render() {
+        this.viewport.render();
     }
 }
--- src/viewport.ts
+++ src/viewport.ts
@@ -1,8 +1,13 @@
+import { Ray } from "./ray";
+import { Vec3 } from "./vec3";
+import { Sphere } from "./sphere";
+
 export class Viewport {
     private width: number
     private height: number
     private canvas: HTMLCanvasElement
     private context: CanvasRenderingContext2D
+    private sphere: Sphere
 
     constructor(parent: HTMLElement, width: number, height: number) {
         this.width = width;
@@ -15,6 +20,8 @@ export class Viewport {
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         this.clear();
+
+        this.sphere = new Sphere(new Vec3(0, 0, 1), 1);
     }
 
     clear() {
@@ -30,6 +37,31 @@ export class Viewport {
             imageData.data[i+1] = Math.random() * 255;
             imageData.data[i+2] = Math.random() * 255;
             i += 4;
+        }
+        this.context.putImageData(imageData, 0, 0);
+    }
+
+    render() {
+        // console.log('loller')
+
+        let imageData = this.context.getImageData(0, 0, this.width, this.height);
+
+        for (var y: number = 0; y < this.height; y++) {
+            for (var x: number = 0; x < this.width; x++) {
+                // console.log('loller2')
+
+                let dirX: number = (1 / this.width) * x;
+                let dirY: number = (1 / this.width) * y;
+
+                let ray: Ray = new Ray(new Vec3(0,0,0), new Vec3(dirX, dirY, 1));
+
+                if (this.sphere.intersect(ray)) {
+                    // console.log('loller3')
+                    imageData.data[x * y * 4 + 0] = 255;
+                    imageData.data[x * y * 4 + 1] = 255;
+                    imageData.data[x * y * 4 + 2] = 255;
+                }
+            }
         }
         this.context.putImageData(imageData, 0, 0);
     }
