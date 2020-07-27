fdc764fec7bbb0cc67d2cbca64d66d1cf6c858cc
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-28 00:45:21 +0900

    Stub implementation of intersect method

--- src/sphere.ts
+++ src/sphere.ts
@@ -1,11 +1,18 @@
-import { Vec3 } from './vec3'
+import { Primitive } from './primitive'
 
-export class Sphere {
+import { Vec3 } from './vec3'
+import { Ray } from './ray'
+
+export class Sphere implements Primitive{
     origin: Vec3
     radius: number
 
     constructor(origin: Vec3, radius: number) {
         this.origin = origin;
         this.radius = radius;
+    }
+
+    intersect(ray: Ray): boolean {
+        return true;
     }
 }
