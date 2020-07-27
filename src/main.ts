fa4f0ce8501f73257d50d29696655c5772fff991
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-27 23:56:32 +0900

    Add buttons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import { Raytracer } from './raytracer';
 
 let raytracer = new Raytracer(640, 480);
-raytracer.fillRandom();
--- src/raytracer.ts
+++ src/raytracer.ts
@@ -1,13 +1,31 @@
 import { Viewport } from './viewport'
 
 export class Raytracer {
-    private div: HTMLElement;
     private viewport: Viewport;
+
+    private div: HTMLDivElement;
+    private clearButton: HTMLButtonElement;
+    private fillRandomButton: HTMLButtonElement;
 
     constructor(width: number, height: number) {
         this.div = document.createElement('div');
         document.body.append(this.div)
         this.viewport = new Viewport(this.div, width, height);
+
+        this.clearButton = document.createElement('button');
+        this.clearButton.innerText = 'Clear';
+        this.clearButton.onclick = ()=>{ this.clear() };
+        this.div.appendChild(this.clearButton);
+
+        this.fillRandomButton = document.createElement('button');
+        this.fillRandomButton.innerText = 'fillRandom';
+        this.fillRandomButton.onclick = ()=>{ this.fillRandom() };
+        this.div.appendChild(this.fillRandomButton);
+
+    }
+
+    clear() {
+        this.viewport.clear();
     }
 
     fillRandom() {
