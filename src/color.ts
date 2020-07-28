7d18b5f060f38d8bc4dcf31dc0fe456809cf2611
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-28 22:04:39 +0900

    Color

---
+++ src/color.ts
@@ -0,0 +1,13 @@
+import { Vec3 } from "./vec3";
+
+export class Color {
+    r: number;
+    g: number;
+    b: number;
+
+    constructor(r:number, g:number, b:number) {
+        this.r = r;
+        this.g = b;
+        this.b = b
+    }
+}
