736353b10babd9b97199d2c06f1ded4745c62c39
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-28 20:47:43 +0900

    Ray-Sphere Algebraic Solution explanation

--- src/sphere.ts
+++ src/sphere.ts
@@ -12,6 +12,28 @@ export class Sphere implements Primitive{
         this.radius = radius;
     }
 
+    /**
+     * Ray-Sphere Intersection (using the Algebraic Solution)
+     *
+     * Ray: r_o + r_d * t
+     * Sphere: (x_s - x_c)^2 + (y_s - y_c)^2 + (z_s - z_c)^2 = r^2
+     *
+     * (x_o + x_d * t - x_c)^2 + (y_o + y_d * t - y_c)^2 + (z_o + z_d * t - z_c)^2 = r^2
+     *
+     * solve for t
+     * A * t^2 + B * t + C (quadratic equation)
+     * where
+     * A = x_d^2 + y_d^2 + z_d^2
+     * B = 2 * (x_d * (x_0 - x_c)+ y_d * (y_0 - y_c) + z_d * (z_o - z_c))
+     * C = (x_o - x_c)^2 + (y_o - y_c)^2 + (z^o - z^c)^2 - r^2
+     *
+     * if the quadratic equation discriminant (B^2 - 4AC)
+     *  > 0 then intersect in two points
+     *  = 0 then intersect in one point
+     *  < 0 then there is no intersection
+     *
+     * Use the quadratic formula to find t
+     */
     intersect(ray: Ray): boolean {
         return true;
     }
