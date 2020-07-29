28c845b4e3d42d6a2bd10cf1760411cf09ccff2d
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-29 20:33:23 +0900

    Add new colors

--- src/color.ts
+++ src/color.ts
@@ -10,4 +10,16 @@ export class Color {
         this.g = g;
         this.b = b;
     }
+
+    public static Red(): Color {
+        return new Color(231, 76, 60)
+    }
+
+    public static Black(): Color {
+        return new Color(44, 62, 80)
+    }
+
+    public static Magenta(): Color {
+        return new Color(155, 89, 182);
+    }
 }
