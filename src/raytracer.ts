75752bcebf4ac452057565510dda7547a2660e9b
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-07-30 23:20:42 +0900

    Separate logic & UI

--- src/raytracer.ts
+++ src/raytracer.ts
@@ -3,51 +3,23 @@ import { Viewport } from './viewport'
 export class Raytracer {
     private viewport: Viewport;
 
-    private div: HTMLDivElement;
-    private clearButton: HTMLButtonElement;
-    private fillRandomButton: HTMLButtonElement;
-    private renderButton: HTMLButtonElement;
-    private saveToPNGButton: HTMLButtonElement;
-
     constructor(width: number, height: number) {
-        this.div = document.createElement('div');
-        document.body.append(this.div)
-        this.viewport = new Viewport(this.div, width, height);
-
-        this.clearButton = document.createElement('button');
-        this.clearButton.innerText = 'Clear';
-        this.clearButton.onclick = () => this.clear();
-        this.div.appendChild(this.clearButton);
-
-        this.fillRandomButton = document.createElement('button');
-        this.fillRandomButton.innerText = 'fillRandom';
-        this.fillRandomButton.onclick = () => this.fillRandom();
-        this.div.appendChild(this.fillRandomButton);
-
-        this.renderButton = document.createElement('button');
-        this.renderButton.innerText = 'render';
-        this.renderButton.onclick = () => this.render();
-        this.div.appendChild(this.renderButton);
-
-        this.saveToPNGButton = document.createElement('button');
-        this.saveToPNGButton.innerText = 'saveToPNG';
-        this.saveToPNGButton.onclick = () => this.saveToPNG();
-        this.div.appendChild(this.saveToPNGButton);
+        this.viewport = new Viewport(width, height);
     }
 
-    clear() {
-        this.viewport.clear();
+    Clear() {
+        this.viewport.Clear();
     }
 
-    fillRandom() {
-        this.viewport.fillRandom();
+    FillRandom() {
+        this.viewport.FillRandom();
     }
 
-    render() {
-        this.viewport.render();
+    Render() {
+        this.viewport.Render();
     }
 
-    saveToPNG() {
-        this.viewport.saveToPNG();
+    GetBuffer(): Uint8ClampedArray {
+        return this.viewport.GetBuffer();
     }
 }
---
+++ src/ui.ts
@@ -0,0 +1,62 @@
+export class UI {
+    private div: HTMLDivElement;
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D
+    private clearButton: HTMLButtonElement;
+    private fillRandomButton: HTMLButtonElement;
+    private renderButton: HTMLButtonElement;
+    private saveToPNGButton: HTMLButtonElement;
+
+    constructor(width: number, height: number) {
+        this.div = document.createElement('div');
+        document.body.append(this.div)
+
+        this.canvas = document.createElement('canvas');
+        this.div.appendChild(this.canvas);
+
+        this.context = this.canvas.getContext('2d');
+        this.canvas.width = width;
+        this.canvas.height = height;
+
+        this.clearButton = document.createElement('button');
+        this.clearButton.innerText = 'Clear';
+        this.div.appendChild(this.clearButton);
+
+        this.fillRandomButton = document.createElement('button');
+        this.fillRandomButton.innerText = 'fillRandom';
+        this.div.appendChild(this.fillRandomButton);
+
+        this.renderButton = document.createElement('button');
+        this.renderButton.innerText = 'render';
+        this.div.appendChild(this.renderButton);
+
+        this.saveToPNGButton = document.createElement('button');
+        this.saveToPNGButton.innerText = 'saveToPNG';
+        this.div.appendChild(this.saveToPNGButton);
+    }
+
+    SetClearButtonOnClick(fn:() => void) {
+        this.clearButton.onclick = fn;
+    }
+
+    SetFillRandomButtonOnClick(fn:() => void) {
+        this.fillRandomButton.onclick = fn;
+    }
+
+    SetRenderButtonOnClick(fn:() => void) {
+        this.renderButton.onclick = fn;
+    }
+
+    SetSaveToPNGButtonOnClick(fn:() => void) {
+        this.saveToPNGButton.onclick = fn;
+    }
+
+    DrawCanvas(buffer: Uint8ClampedArray) {
+        this.context.putImageData(new ImageData(buffer, this.canvas.width, this.canvas.height), 0, 0);
+    }
+
+    SaveToPNG() {
+        var newTab = window.open();
+        newTab.document.body.innerHTML = '<img src="' + this.canvas.toDataURL() + '">';
+    }
+}
