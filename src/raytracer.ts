06ec931bf639971b99db63686b4fa8f154787a05
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-08-09 19:13:28 +0900

    RenderXY

--- src/raytracer.ts
+++ src/raytracer.ts
@@ -45,6 +45,31 @@ export class Raytracer {
         }
     }
 
+    RenderXY(x: number, y: number) {
+        console.log(`Coordinates x: ${x}, x: ${y}`);
+        let ray: Ray = this.viewport.GetRay(x, y);
+        console.log(`Ray x: ${ray.direction.x}, y: ${ray.direction.y}, z: ${ray.direction.z}`);
+
+        this.scene.Objects().forEach(object => {
+            let hitInfo: HitInfo = object.intersect(ray);
+
+            if (hitInfo.hit) {
+                let pointToLight = this.scene.Light().position.Subtract(hitInfo.hitPoint).Normalized();
+
+                let intensity = Math.max(0, pointToLight.Dot(hitInfo.normal));
+
+                let hitInfoColor: Color = hitInfo.GetColor();
+                hitInfoColor = new Color(hitInfoColor.r * intensity, hitInfoColor.g * intensity, hitInfoColor.b * intensity);
+
+                console.log(`Color r: ${hitInfoColor.r}, g: ${hitInfoColor.g}, b: ${hitInfoColor.b}`);
+                console.log(`Normal x: ${hitInfo.normal.x}, y: ${hitInfo.normal.y}, z: ${hitInfo.normal.z}`);
+            } else {
+                console.log('NO HIT');
+            }
+            console.log('-----');
+        });
+    }
+
     GetBuffer(): Uint8ClampedArray {
         return this.viewport.GetBuffer();
     }
