ec64710e2bdbe4cb681bddecab29533a15236855
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-08-10 15:15:39 +0900

    Refactor Render() method - first pass

--- src/ray.ts
+++ src/ray.ts
@@ -1,8 +1,12 @@
 import { Vec3 } from "./vec3";
+import { HitInfo } from './hitinfo';
+import { Scene } from "./scene";
+import { Color } from "./color";
 
 export class Ray {
     origin : Vec3;
     direction : Vec3;
+    public hitInfo : HitInfo;
 
     constructor(origin: Vec3, direction: Vec3) {
         this.origin = origin;
@@ -11,5 +15,27 @@ export class Ray {
 
     public CalcPointAtT(t: number): Vec3 {
         return this.origin.Add(this.direction.ScalarMultiply(t));
+    }
+
+    //TODO: find a better name for this method
+    public Shoot(scene: Scene): Color {
+        let color: Color = null;
+        for (let i = 0; i < scene.Objects().length; i++) {
+
+            const object = scene.Objects()[i];
+            this.hitInfo = object.intersect(this);
+
+            if (this.hitInfo.hit) {
+                let pointToLight = scene.Light().position.Subtract(this.hitInfo.hitPoint).Normalized();
+
+                let intensity = Math.max(0, pointToLight.Dot(this.hitInfo.normal));
+
+                let hitInfoColor: Color = this.hitInfo.GetColor();
+
+                color = new Color(hitInfoColor.r * intensity, hitInfoColor.g * intensity, hitInfoColor.b * intensity);
+                break; //TODO: intersect with multiple objects is not supported
+            }
+        }
+        return color;
     }
 }
--- src/raytracer.ts
+++ src/raytracer.ts
@@ -22,24 +22,13 @@ export class Raytracer {
     }
 
     Render() {
-        // this.viewport.Render(this.scene);
         for (var y: number = 0; y < this.viewport.Height(); y++) {
             for (var x: number = 0; x < this.viewport.Width(); x++) {
                 let ray: Ray = this.viewport.GetRay(x, y);
-
-                this.scene.Objects().forEach(object => {
-                    let hitInfo: HitInfo = object.intersect(ray);
-
-                    if (hitInfo.hit) {
-                        let pointToLight = this.scene.Light().position.Subtract(hitInfo.hitPoint).Normalized();
-
-                        let intensity = Math.max(0, pointToLight.Dot(hitInfo.normal));
-
-                        let hitInfoColor: Color = hitInfo.GetColor();
-
-                        this.viewport.DrawPixel(x, y, new Color(hitInfoColor.r * intensity, hitInfoColor.g * intensity, hitInfoColor.b * intensity));
-                    }
-                });
+                let color: Color = ray.Shoot(this.scene);
+                if (color) {
+                    this.viewport.DrawPixel(x, y, color);
+                }
             }
         }
     }
