c46438f59a74f80b686ed113950b97898814f9a7
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-08-11 13:31:00 +0900

    Add intersection depth sorting

--- test/cameraray.test.ts
+++ test/cameraray.test.ts
@@ -1,5 +1,7 @@
 import { Vec3 } from '../src/vec3'
 import { CameraRay } from '../src/cameraray'
+import { HitInfo } from '../src/hitinfo';
+import { Color } from '../src/color';
 
 test('Initialize the ray', () => {
     let origin    = new Vec3(0, 0, 0);
@@ -24,3 +26,29 @@ test('Tests ray at t = 1', ()=> {
     expect(pointAtT.y).toBe(0);
     expect(pointAtT.z).toBe(0);
 });
+
+test('Add HitInfo', ()=>{
+    let hitInfo = new HitInfo(true, 1, Vec3.Zero(), Vec3.Zero(), Color.White());
+    let ray = new CameraRay(Vec3.Zero(), Vec3.Forward());
+    ray.AddHit(hitInfo);
+
+    expect(ray.GetNearerHit()).toBe(hitInfo);
+})
+
+test('GetNearerHit', ()=>{
+    let hitInfo1 = new HitInfo(true, 1,   Vec3.Zero(), Vec3.Zero(), Color.White());
+    let hitInfo2 = new HitInfo(true, 2,   Vec3.Zero(), Vec3.Zero(), Color.White());
+    let hitInfo3 = new HitInfo(true, 0.5, Vec3.Zero(), Vec3.Zero(), Color.White());
+    let ray = new CameraRay(Vec3.Zero(), Vec3.Forward());
+    ray.AddHit(hitInfo1);
+    ray.AddHit(hitInfo2);
+    ray.AddHit(hitInfo3);
+
+    expect(ray.GetNearerHit()).toBe(hitInfo3);
+})
+
+test('Return null if no hits', ()=>{
+    let ray = new CameraRay(Vec3.Zero(), Vec3.Forward());
+
+    expect(ray.GetNearerHit()).toBeNull();
+})
