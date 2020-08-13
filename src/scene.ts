bef0d9bb4c3bf7bfb03ae222784b5f848ebb5c4e
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-08-13 17:12:53 +0900

    Setting: enable/disable objects

--- src/scene.ts
+++ src/scene.ts
@@ -4,6 +4,7 @@ import { IIntersectable } from "./iintersectable"
 import { Sphere } from "./sphere"
 import { Light } from "./light"
 import { Plane } from "./plane"
+import { Settings } from "./settings"
 
 export class Scene {
     private light: Light
@@ -13,11 +14,19 @@ export class Scene {
     constructor() {
         this.objects = new Array();
 
-        this.light = new Light(new Vec3(1000, 500, 3))
+        this.light = new Light(new Vec3(1000, 1000, 3))
 
-        this.objects.push(new Sphere(new Vec3(.9, 0.5, 2.4), 0.1, Color.Yellow()));
-        this.objects.push(new Sphere(new Vec3(0, 0, 3), 1, Color.Red()));
-        this.objects.push(new Plane(Vec3.Up(), Color.Green()));
+        if (Settings.RENDER_YELLOW_SPHERE) {
+            this.objects.push(new Sphere(new Vec3(.9, 0.5, 2.4), 0.1, Color.Yellow()));
+        }
+
+        if (Settings.RENDER_RED_SPHERE) {
+            this.objects.push(new Sphere(new Vec3(0, 0, 3), 1, Color.Red()));
+        }
+
+        if (Settings.RENDER_PLANE) {
+            this.objects.push(new Plane(Vec3.Up(), Color.Green()));
+        }
     }
 
     Objects(): Array<IIntersectable> {
--- src/settings.ts
+++ src/settings.ts
@@ -3,4 +3,7 @@ export class Settings {
     public static readonly HEIGHT        = 480;
     public static          DEBUG_NORMALS = false;
     public static          AA            = true;
+    public static          RENDER_YELLOW_SPHERE = true;
+    public static          RENDER_RED_SPHERE    = true;
+    public static          RENDER_PLANE         = true;
 }
