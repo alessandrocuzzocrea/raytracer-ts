bc474a99f09e11e199f469ef5cf090748f7cb0e0
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-08-13 17:20:54 +0900

    Fix indentation

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 export class Settings {
-    public static readonly WIDTH         = 640;
-    public static readonly HEIGHT        = 480;
-    public static          DEBUG_NORMALS = false;
-    public static          AA            = true;
+    public static readonly WIDTH                = 640;
+    public static readonly HEIGHT               = 480;
+    public static          DEBUG_NORMALS        = false;
+    public static          AA                   = true;
     public static          RENDER_YELLOW_SPHERE = true;
     public static          RENDER_RED_SPHERE    = true;
     public static          RENDER_PLANE         = true;
